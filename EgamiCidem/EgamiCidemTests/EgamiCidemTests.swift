aee2c2617840059e39f9ca06ec68bd8bc6e19bc9
ihefe－hulinhua <ihefe-hulinhua@ihefehulinhuadeMacBook-Air.local>
2016-05-10 15:53:44 +0800

    creat swift_medic_image_demo

---
+++ EgamiCidem/EgamiCidemTests/EgamiCidemTests.swift
@@ -0,0 +1,36 @@
+//
+//  EgamiCidemTests.swift
+//  EgamiCidemTests
+//
+//  Created by ihefe－hulinhua on 16/5/10.
+//  Copyright © 2016年 ihefe_hlh. All rights reserved.
+//
+
+import XCTest
+@testable import EgamiCidem
+
+class EgamiCidemTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
